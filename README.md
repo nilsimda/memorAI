bcaac3491aea3fc4d154887cf32bd0d799e8935c
Nils Imdahl <imdahlnils@gmail.com>
2022-04-22 18:58:21 +0200

    fix readme and use argparser for nicer cli with speec2text

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#About
+## About
 MemorAI is a project developed as part of the TUM-AI Makeathon. It tries to implement
 an AI-Assistant, that utilizes reminiscence therapy to help dementia patients.
--- user_feedback/speech2text.py
+++ user_feedback/speech2text.py
@@ -2,16 +2,20 @@
 A short script that takes a .wav file and converts it to text
 """
 
+from argparse import ArgumentParser
 from transformers import pipeline
 
 def main():
+    parser = ArgumentParser(description="Process the provided audio file to text")
+    parser.add_argument("--input", type=str, help="The input file to process")
+
     asr = pipeline(
             task="automatic-speech-recognition",
             model= "facebook/wav2vec2-base-960h",
             tokenizer= "facebook/wav2vec2-base-960h",
             )
-
-    text = asr("test.wav")["text"]
+    args = parser.parse_args()
+    text = asr(args.input)["text"]
     print(text)
 
 if __name__ == "__main__":
