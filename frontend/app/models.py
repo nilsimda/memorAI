e713e8bd61573edaa63a00ad603701055e1c3192
Nils Imdahl <imdahlnils@gmail.com>
2022-04-23 16:10:40 +0200

    login, added two users Nils and Juan

--- frontend/app/models.py
+++ frontend/app/models.py
@@ -1,6 +1,8 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
@@ -14,6 +16,12 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class Feedback(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     feedback = db.Column(db.String(140))
@@ -23,3 +31,8 @@ class Feedback(db.Model):
 
     def __repr__(self):
         return '<Feedback {}>'.format(self.feedback)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
