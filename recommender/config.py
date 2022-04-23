f82c7a5266067577a0367da2e25c06ab6c2abe76
Juan Galvis <jdgalviss@gmail.com>
2022-04-23 02:44:24 +0200

    feat: GPT-3 Simple recommendations

---
+++ recommender/config.py
@@ -0,0 +1,6 @@
+OPENAI_API_KEY="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"
+GCS_DEVELOPER_KEY="AIzaSyD6YrPAxj2XvA1DfLiuXxkH359Pxwhha2M"
+GCS_CX="96bc99f9c157f4538"
+# <script async src="https://cse.google.com/cse.js?cx=96bc99f9c157f4538">
+# </script>
+# <div class="gcse-search"></div>
---
+++ recommender/env.sh
@@ -0,0 +1,4 @@
+export GCS_DEVELOPER_KEY=AIzaSyD6YrPAxj2XvA1DfLiuXxkH359Pxwhha2M
+export GCS_CX=96bc99f9c157f4538
+
+# gimages -k AIzaSyD6YrPAxj2XvA1DfLiuXxkH359Pxwhha2M -c secret-grammar-155502 search -q puppies
